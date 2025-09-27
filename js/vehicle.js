80a3d5e7a1616cc521f1d12929dd340e33ba6795
Alex Rabarts <alexrabarts@gmail.com>
2025-09-27 17:28:48 +0100

    Revert "Scale turn rate by steering input into corner for stronger, proportional turning"
    This reverts commit eb2973e72ccc13af4acaec20dc1b8ed8d719aba2.

--- js/vehicle.js
+++ js/vehicle.js
@@ -206,8 +206,10 @@ class Vehicle {
             
             const angularVel = this.speed / actualRadius;
             // Require steering into the corner after countersteering
-            const turnMultiplier = Math.max(0, steeringInput * Math.sign(this.leanAngle));
-            this.yawAngle -= angularVel * Math.sign(this.leanAngle) * turnMultiplier * deltaTime;
+            const steerIntoCorner = steeringInput * Math.sign(this.leanAngle) > 0.1; // Small threshold
+            if (steerIntoCorner) {
+                this.yawAngle -= angularVel * Math.sign(this.leanAngle) * deltaTime;
+            }
         }
         
         // Debug output
